cc31fdf4907973f3e196d9982ccbd74c4d77959c
Bruno Rocha de Lima <brunorocha33@live.com>
2022-06-22 09:38:50 -0300

    input para o usuário ter a habilidade de buscar por restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,46 +1,92 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { IPaginacao } from '../../interfaces/IPaginacao';
-import IRestaurante from '../../interfaces/IRestaurante';
-import style from './ListaRestaurantes.module.scss';
-import Restaurante from './Restaurante';
+import { Button, TextField } from "@mui/material";
+import axios, { AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+import { IPaginacao } from "../../interfaces/IPaginacao";
+import IRestaurante from "../../interfaces/IRestaurante";
+import style from "./ListaRestaurantes.module.scss";
+import Restaurante from "./Restaurante";
 
-const ListaRestaurantes = () => {
-
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
-  const [ proximaPagina, setProximaPagina] = useState('')
-  const [paginaAnterior, setPaginaAnterior] = useState('')
-
-  useEffect(() => {
-    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
-    .then(response => {
-      setRestaurantes(response.data.results)
-      setProximaPagina(response.data.next)
-    })
-    .catch(error => {console.log(error)})
-  }, [])
-  
-  function carregarDados(url: string) {
-    axios.get<IPaginacao<IRestaurante>>(url)
-    .then(response => {
-      setRestaurantes(response.data.results)
-      setProximaPagina(response.data.next)
-      setPaginaAnterior(response.data.previous)
-    })
-    .catch(error => {console.log(error)})
-  }
-
-  
-  return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
-      Página Anterior
-    </button>}
-    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
-      Próxima página
-    </button>}
-  </section>)
+// esses são os possíveis parâmetros que podemos enviar para a API
+interface IParametrosBusca {
+  ordering?: string;
+  search?: string;
 }
 
-export default ListaRestaurantes
+const ListaRestaurantes = () => {
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState("");
+  const [paginaAnterior, setPaginaAnterior] = useState("");
+  const [busca, setBusca] = useState("");
+
+  // agora, o carregarDados recebe opcionalmente as opções de configuração do axios
+  function carregarDados(url: string, opcoes: AxiosRequestConfig = {}) {
+    axios
+      .get<IPaginacao<IRestaurante>>(url, opcoes)
+      .then((response) => {
+        setRestaurantes(response.data.results);
+        setProximaPagina(response.data.next);
+        setPaginaAnterior(response.data.previous);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  // a cada busca, montamos um objeto de opções
+  function buscar(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    const opcoes = {
+      params: {} as IParametrosBusca,
+    };
+    if (busca) {
+      opcoes.params.search = busca;
+    }
+    carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
+  }
+
+  useEffect(() => {
+    // obter restaurantes
+    carregarDados("http://localhost:8000/api/v1/restaurantes/");
+  }, []);
+
+
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>
+        Os restaurantes mais <em>bacanas</em>!
+      </h1>
+      <form onSubmit={buscar}>
+        <TextField
+          label="Buscar Restaurante"
+          variant="standard"
+          value={busca}
+          onChange={(evento) => setBusca(evento.target.value)}
+        />
+        <Button type="submit" variant="outlined">Buscar</Button>
+      </form>
+      {restaurantes?.map((item) => (
+        <Restaurante restaurante={item} key={item.id} />
+      ))}<br/>
+      {
+        <Button style={{marginRight: "1rem"}}
+          variant="contained"
+          onClick={() => carregarDados(paginaAnterior)}
+          disabled={!paginaAnterior}
+        >
+          Página Anterior
+        </Button> 
+      }
+      {
+        <Button
+          variant="contained"
+          onClick={() => carregarDados(proximaPagina)}
+          disabled={!proximaPagina}
+        >
+          Próxima página
+        </Button>
+      }
+    </section>
+  );
+};
+
+export default ListaRestaurantes;
